4799bdae6be4b8531cf22e4ad4cef2ca29cecc28
luid <eng.limaluis@gmail.com>
2024-09-22 20:49:38 -0300

    feature: adding comentory about original material

--- services/frontend/index.js
+++ services/frontend/index.js
@@ -88,3 +88,14 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error(err);
         });
 });
+
+
+
+
+/*  
+
+Material traduzido e adaptado de:
+Marco Tulio Valente. Engenharia de Software Moderna: Princípios e Práticas para Desenvolvimento de Software com Produtividade,     Editora: Independente, 2020.
+UnB-FGA-EPS-MDS
+ 
+*/
